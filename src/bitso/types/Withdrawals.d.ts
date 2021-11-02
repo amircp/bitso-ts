b70c6bbf4864416aafb7419dbb8cda531be1808f
Amir <hello@amircanto.com>
2021-11-02 16:59:29 -0600

    Restore Trades.d.ts

---
+++ src/bitso/types/Withdrawals.d.ts
@@ -0,0 +1,31 @@
+export type txInfo = {
+    withdrawal_address: string,
+    tx_hash: string | null
+}
+
+export type speiInfo = {
+    sender_name: string,
+    receive_clabe: string,
+    sender_clabe: string,
+    numeric_reference: string,
+    concepto: string,
+    clave_rastreo: string | null,
+    beneficiary_name: string
+}
+
+export type TWithDrawal = {
+    wid: string,
+    status: string,
+    created_at: string,
+    currency: string,
+    method: string,
+    amount: string,
+    details: txInfo,
+    status?: string
+}
+
+export type TBankCode = {
+    code: string,
+    name: string
+}
+
